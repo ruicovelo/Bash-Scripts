7b513f18e524bf6f684e0a2a366ebb48633f56ca
ruicovelo <rui.covelo@gmail.com>
2014-08-25 09:34:30 +0100

    Adding a dumb wait for file to exit when file does not exist

--- logmonitor/logmonitor.sh
+++ logmonitor/logmonitor.sh
@@ -2,6 +2,8 @@
 
 LOG_FILE=$1
 
+clear
+while [ ! -f $LOG_FILE ]; do echo "File does not exist..."; echo "Waiting for it..."; sleep 2; clear; done 
 tail -f "$LOG_FILE" | \
 awk ' \
 /^ERROR/ {print "\033[31m" $0 "\033[39m"; print "\a"; next};
